eded570bf60500302f8c8b6439286739f85aafaf
Tiago <tiagohjcarvalho@gmail.com>
2026-05-07 20:08:32 +0100

    initialization of the project

---
+++ structure.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import random
+import utils
+
+
+
+def generate_solution(size, lower_bound, upper_bound):
+    """
+    Parameters
+    ----------
+    size->int: 
+
+    lower_bound->float
+
+    
+    Output
+    ----------
+    
+    """
+    
+
+
+
+    return [random.uniform(lower_bound,upper_bound) for weight in range(size)]
+
+
