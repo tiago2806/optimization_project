dc2293ebacd7f5c1885219ee5c77d44e3b012217
Tiago <tiagohjcarvalho@gmail.com>
2026-06-05 22:03:31 +0100

    finito

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -9,9 +9,6 @@ def genetic_algorithm(generate_solution, fitness_function, selection,
                       crossover, mutation, pop_size, n_generations, mutation_rate,
                       verbose=True):
  
-    """
-    docstring a explicar o objectivo da função e os parameters e os outputs
-    """
     
     #Step 1: initialise the population and evaluate it
     population = initialize_pop(generate_solution, pop_size)
